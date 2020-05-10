f85c2803777e155f5e62666edca8e0c48ab157c3
Sachin Jangid <34537025+gittysachin@users.noreply.github.com>
2020-05-10 12:33:01 +0530

    Simulating a Slow Request in the Current Server Implementation

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use std::net::TcpListener;
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::fs;
-// Turning Our Single-Threaded Server into a Multithreaded Server"
+use std::thread;
+use std::time::Duration;
 
 fn main(){
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -18,12 +19,18 @@ fn handle_connection(mut stream: TcpStream){
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
 
-    // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
-
     let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    // If the server receives a request that takes a long time to process, subsequent requests will have to wait until the long request is finished, even if the new requests can be processed quickly.
+
+    // Open two browser windows: one for http://127.0.0.1:7878/ and the other for http://127.0.0.1:7878/sleep. If you enter the / URI a few times, as before, you’ll see it respond quickly. But if you enter /sleep and then load /, you’ll see that / waits until sleep has slept for its full 5 seconds before loading.
 
     let (status_line, filename) = if buffer.starts_with(get) {
         ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 404 OK\r\n\r\n", "hello.html")
     } else {
         ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
     };
